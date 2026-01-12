e5e840eb585da847dbb2ee5afafacd6c17ccb178
Daroueche MARI <daroueche.mari@laplateforme.io>
2026-01-12 13:30:06 +0100

    Add files via upload

---
+++ pascaline(addition daroueche).py
@@ -0,0 +1,8 @@
+# addition
+def addition():
+    val1 = float(input("Choisissez un chiffre ou nombre :"))
+    val2 = float(input("Choisissez un deuxieme chiffre ou nombre :"))
+    result = val1 + val2
+    print(result)
+
+addition()
