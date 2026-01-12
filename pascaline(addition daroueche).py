4708b858114b7f65dbd5e152ae31fe4ef0c31270
Daroueche MARI <daroueche.mari@laplateforme.io>
2026-01-12 13:31:19 +0100

    Delete pascaline(addition daroueche).py

--- pascaline(addition daroueche).py
+++
@@ -1,8 +0,0 @@
-# addition
-def addition():
-    val1 = float(input("Choisissez un chiffre ou nombre :"))
-    val2 = float(input("Choisissez un deuxieme chiffre ou nombre :"))
-    result = val1 + val2
-    print(result)
-
-addition()
