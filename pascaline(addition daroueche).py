dc0947d9f6d620aa25e3671355623e078671a3a9
Daroueche MARI <daroueche.mari@laplateforme.io>
2026-01-12 13:45:53 +0100

    Try/Except ajouté

--- pascaline(addition daroueche).py
+++ pascaline(addition daroueche).py
@@ -4,5 +4,10 @@ def addition():
     val2 = float(input("Choisissez un deuxieme chiffre ou nombre :"))
     result = val1 + val2
     print(result)
+try:
+        val1 = float(input("Choisissez un chiffre ou nombre :"))
+        val2 = float(input("Choisissez un deuxieme chiffre ou nombre :"))
+except:
+      print("Erreur veuillez entrer un chiffre ou nombre")
 
 addition()
