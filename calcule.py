837e48d95baa358c528d0b2c6c354c153e4473fc
ceciliaperana13 <cperana13@gmail.com>
2026-01-12 14:31:51 +0100

    def refais

--- calcule.py
+++ calcule.py
@@ -3,3 +3,6 @@ def puissance(base,exposant):
     exposant = int(input("Entrez l'exposant (entier) : "))
     resultat = base ** exposant
     print(f"{base} élevé à la puissance {exposant} est égal à {resultat}")
+
+
+    
