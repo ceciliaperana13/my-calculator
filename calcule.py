997572146c5fafbb2fa143e19c6a3f211abcafa6
ceciliaperana13 <cperana13@gmail.com>
2026-01-12 12:49:33 +0100

    def puissance en construction

---
+++ calcule.py
@@ -0,0 +1,5 @@
+def puissance(base,exposant):
+    base = float(input("Entrez la base : "))
+    exposant = int(input("Entrez l'exposant (entier) : "))
+    resultat = base ** exposant
+    print(f"{base} élevé à la puissance {exposant} est égal à {resultat}")
