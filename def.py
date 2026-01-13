1dfc466d9fbd2e1f5f69d7d3c07bed596d44e8f1
ceciliaperana13 <cperana13@gmail.com>
2026-01-13 07:46:28 +0100

    essaie de calculatrice

---
+++ def.py
@@ -0,0 +1,19 @@
+def addition(self, a, b):
+        
+        return a + b
+    
+def soustraction(self, a, b):
+        
+        return a - b
+    
+def multiplication(self, a, b):
+        
+        return a * b
+    
+def division(self, a, b):
+       
+        if b == 0:
+            raise ZeroDivisionError("Division par zéro impossible")
+        return a / b
+
+       
